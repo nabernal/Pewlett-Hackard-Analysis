3077e8e4af25e22ef9e8bcdb40347579c0a87b6e
nabernal <america94bernal@gmail.com>
2022-10-27 00:02:27 -0700

    Updated Schema and Qury files

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -1,60 +1,3 @@
--- Creating tables for PH-EmployeeDB
-CREATE TABLE departments (
-	dept_no VARCHAR(4) NOT NULL,
-	dept_name VARCHAR(40) NOT NULL,
-	PRIMARY KEY (dept_no),
-	UNIQUE (dept_name)
-);
-
-CREATE TABLE employees (
-	emp_no INT NOT NULL,
-	birth_date DATE NOT NULL,
-	first_name VARCHAR NOT NULL,
-	last_name VARCHAR NOT NULL,
-	gender VARCHAR NOT NULL,
-	hire_date DATE NOT NULL,
-	PRIMARY KEY (emp_no)
-);
-
-CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
-	emp_no INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no, dept_no)
-);
-
-CREATE TABLE dept_emp (
-	emp_no INT NOT NULL,
-	dept_no VARCHAR(4) NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no, dept_no)
-);
-
-CREATE TABLE titles (
-	emp_no INT NOT NULL,
-	title VARCHAR(50) NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no, title, from_date)
-);
-
-
-CREATE TABLE salaries (
-	emp_no INT NOT NULL,
-	salary INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
-);
-
 SELECT * FROM departments;
 
 
@@ -103,6 +46,7 @@ AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 -- How can we see what the table looks like? 
 SELECT * FROM retirement_info;
 
+
 -----------Module 7.3.2-----------
 -- Drop retirement table to join other tables first
 DROP TABLE retirement_info;
@@ -116,7 +60,6 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date Between '1985-01-01' AND '1988-12-31');
 --Check the table
 SELECT * FROM retirement_info;
-
 
 -- Joining departments and dept_manager tables using INNER JOIN
 SELECT departments.dept_name,
